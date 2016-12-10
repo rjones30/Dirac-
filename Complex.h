8a3dfc1c22a913dcbe3ee923678e37e28fe6f89a
Richard Jones <rjones30@gmail.com>
2016-12-10 15:31:12 -0500

    * Complex.h [rtj]    - modify the Complex_t type to use extended precision (LDouble_t)      in its internal represention of the real,imag parts.

--- Complex.h
+++ Complex.h
@@ -1,7 +1,8 @@
 #ifndef ROOT_Complex_t
 #define ROOT_Complex_t 1
 
+#include "Double.h"
 #include <complex>
-typedef std::complex<double> Complex_t;
+typedef std::complex<LDouble_t> Complex_t;
 
 #endif
