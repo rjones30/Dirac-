5d8f92266961bebba38ec588d058a2c75773d66c
Richard Jones <rjones30@gmail.com>
2016-12-10 15:32:56 -0500

    * constants.h [rtj]    - move the constants over to extended precision (LDouble_t)

--- constants.h
+++ constants.h
@@ -1,5 +1,8 @@
 #ifndef physics_constants
 #define physics_constants
-const complex<double> I_(0,1);
-const double PI_=2*atan2(1,0);
+
+#include "Double.h"
+
+const complex<LDouble_t> I_(0,1);
+const LDouble_t PI_=2*atan2(1,0);
 #endif
