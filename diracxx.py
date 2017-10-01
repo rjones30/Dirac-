528ca0041e037a60e39364e6ac1c95051eeba446
Richard Jones <rjones30@gmail.com>
2017-09-30 22:49:21 -0400

    * diracxx.py [rtj]    - improve the example usage comments in the module header.

--- diracxx.py
+++ diracxx.py
@@ -5,7 +5,10 @@
 # usage example:
 #   $ python
 #   >>> from diracxx import *
-#   >>> t123 = TThreeVectorReal(1,2,3)
+#   >>> t1 = TThreeVectorReal(1,2,3)
+#   >>> t2 = TThreeVectorComplex((0+1j),(2-5j),(3+2j),(8-4j))
+#   >>> t2 += TThreeVectorComplex(t1)
+#   >>> t2.Print()
 #
 # author: richard.t.jones at uconn.edu
 # version: august 16, 2017
